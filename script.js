a2c42a87c8cd6d8e34dd71d17dbc2088df7f04ae
Herr13032006 <hjiroa7m@gmail.com>
2024-12-19 09:17:51 +0700

    Update script.js
    mobile responsive

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const header = document.querySelector("header");
+const menu = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
 
 // Toggle menu visibility on small screens
-document.getElementById('menu-icon').addEventListener('click', function() {
-    document.getElementById('navlist').classList.toggle('active');
-  });
-  
+menu.addEventListener("click", () => {
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("active");
+});
 
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky",this.window.scrollY > 120);
-})
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector('.navlist');
+// Sticky header on scroll
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 120);
 
-menu.onclick = () =>{
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
-}
-
-window.onscroll = () =>{
-    menu.remove.toggle('bx-x');
-    navlist.remove.toggle('active');
-}
+    // Close menu on scroll
+    if (window.scrollY > 0) {
+        menu.classList.remove("bx-x");
+        navlist.classList.remove("active");
+    }
+});
